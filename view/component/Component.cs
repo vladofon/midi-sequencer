d354ea056df244ff0010ac2e2985f7d6bdbe1e3a
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-09-22 20:42:07 +0300

    Created a general Interface for all view-components

---
+++ view/component/Component.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace midi_sequencer.view.component
+{
+    internal interface Component
+    {
+        public Grid Draw();
+    }
+}
