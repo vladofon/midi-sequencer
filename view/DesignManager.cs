8c0ef055f4fd706b17c7fe0c369811dc25ead9c5
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-09-22 20:43:13 +0300

    Refactored DesignManager

---
+++ view/DesignManager.cs
@@ -0,0 +1,27 @@
+﻿using midi_sequencer.view.component;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace midi_sequencer
+{
+    internal class DesignManager
+    {
+        public void DrawPianoRoll(Window window)
+        {
+            Component pianoRoll = new PianoRoll();
+
+            ScrollViewer scroll = new ScrollViewer();
+            scroll.HorizontalScrollBarVisibility = ScrollBarVisibility.Visible;
+            scroll.Content = pianoRoll.Draw();
+
+            window.Content = scroll;
+        }
+    }
+}
