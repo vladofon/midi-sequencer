7af1afb2278ae999a8e7e9b1833d946d978cbaa3
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-09-23 15:18:19 +0300

    Added animated tracing for note buttons

--- view/component/PianoRoll.cs
+++ view/component/PianoRoll.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Threading;
 
 namespace midi_sequencer.view.component
 {
@@ -14,6 +15,10 @@ namespace midi_sequencer.view.component
     {
         private Button currentPressedNote = new Button();
         private Point mouseDown = new(0, 0);
+        private bool isNotePressed = false;
+
+        private Dictionary<int, List<Button>> notes = new();
+        private List<Button> noteTrace = new();
 
         private const int noteBtnWidth = 20;
         private const int noteBtnHeight = 12;
@@ -23,13 +28,27 @@ namespace midi_sequencer.view.component
 
         private Grid pianoGrid = new();
 
+        public PianoRoll()
+        {
+            InitPianoGrid();
+        }
+
         public Grid Draw()
         {
             pianoGrid.Background = Brushes.Gray;
             pianoGrid.MouseDown += Piano_MouseDown;
             pianoGrid.MouseUp += Piano_MouseUp;
+            pianoGrid.MouseMove += Piano_MouseMove;
 
             return this.pianoGrid;
+        }
+
+        private void InitPianoGrid()
+        {
+            for (int noteRow = 1; noteRow < countOfNotes; noteRow++)
+            {
+                this.notes.Add(noteRow, new List<Button>());
+            }
         }
 
         private void Piano_MouseDown(object sender, MouseButtonEventArgs e)
@@ -52,22 +71,35 @@ namespace midi_sequencer.view.component
             currentPressedNote.HorizontalAlignment = HorizontalAlignment.Left;
             currentPressedNote.VerticalAlignment = VerticalAlignment.Top;
 
-            currentPressedNote.PreviewMouseDown += note_MouseDown;
-            currentPressedNote.PreviewMouseUp += note_MouseUp;
+            currentPressedNote.PreviewMouseDown += Note_MouseDown;
+            currentPressedNote.PreviewMouseUp += Note_MouseUp;
+            currentPressedNote.PreviewMouseMove += Note_MouseMove;
 
             pianoGrid.Children.Add(currentPressedNote);
+            this.notes[(int)this.mouseDown.Y / noteBtnHeight].Add(currentPressedNote);
+            //currentPressedNote.Content = "" + (int)this.mouseDown.Y / noteBtnHeight;
+            //currentPressedNote.Content = "" + currentPressedNote.Margin.Left;
+
+            this.isNotePressed = true;
         }
 
         private void Piano_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            buildNoteCell(e.GetPosition(pianoGrid).X);
+            BuildNoteCell(e.GetPosition(pianoGrid));
+            this.isNotePressed = false;
         }
 
-        void buildNoteCell(double mouseUp_X)
+        void BuildNoteCell(Point mousePosition)
         {
-            if (mouseUp_X <= this.mouseDown.X) return;
+            if (mousePosition.X <= this.mouseDown.X) return;
 
-            int left = (int)mouseUp_X - (int)mouseUp_X % noteBtnWidth;
+            int left = (int)mousePosition.X - (int)mousePosition.X % noteBtnWidth;
+
+            bool isIntersect = notes[(int)this.currentPressedNote.Margin.Top / noteBtnHeight].Find(
+                i => (this.currentPressedNote.Margin.Left <= i.Margin.Left && left >= i.Margin.Left && !i.Equals(currentPressedNote))
+            ) != null;
+
+            if (isIntersect) return;
 
             if (currentPressedNote.Width == noteBtnWidth)
             {
@@ -79,16 +111,37 @@ namespace midi_sequencer.view.component
             }
         }
 
-        void note_MouseUp(object sender, MouseButtonEventArgs e)
+        void Note_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            buildNoteCell(e.GetPosition(pianoGrid).X);
+            BuildNoteCell(e.GetPosition(pianoGrid));
+            this.isNotePressed = false;
+            this.noteTrace.Clear();
         }
 
-        void note_MouseDown(object sender, MouseButtonEventArgs e)
+        void Note_MouseDown(object sender, MouseButtonEventArgs e)
         {
             this.currentPressedNote = (Button)sender;
             this.mouseDown.X = (int)currentPressedNote.Margin.Left;
             this.mouseDown.Y = (int)currentPressedNote.Margin.Top;
+            this.isNotePressed = true;
+        }
+
+        void Piano_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (this.isNotePressed)
+            {
+                BuildNoteCell(e.GetPosition(pianoGrid));
+            }
+        }
+
+        void Note_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (this.isNotePressed)
+            {
+                BuildNoteCell(e.GetPosition(pianoGrid));
+            }
         }
     }
 }
