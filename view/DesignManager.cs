858bf8e2a087d9ff210afe426e0ae81787720816
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-10-06 13:24:46 +0300

    Link new Components

--- view/DesignManager.cs
+++ view/DesignManager.cs
@@ -1,4 +1,8 @@
-﻿using midi_sequencer.view.component;
+﻿using midi_sequencer.model;
+using midi_sequencer.service;
+using midi_sequencer.view.component;
+using midi_sequencer.view.component.piano_roll;
+using NAudio.Midi;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,16 +17,35 @@ namespace midi_sequencer
 {
     internal class DesignManager
     {
+        PianoRoll pianoRoll = new PianoRoll(1);
+
         public void DrawPianoRoll(Window window)
         {
             window.Show();
-            Component pianoRoll = new PianoRoll();
 
-            ScrollViewer scroll = new ScrollViewer();
-            scroll.HorizontalScrollBarVisibility = ScrollBarVisibility.Visible;
-            scroll.Content = pianoRoll.Build();
+            Grid view = new();
+            ColumnDefinition firstCol = new();
+            RowDefinition firstRow = new RowDefinition();
+            firstRow.Height = new GridLength(30);
+            RowDefinition secondRow = new RowDefinition();
 
-            window.Content = scroll;
+            view.ColumnDefinitions.Add(firstCol);
+            view.RowDefinitions.Add(firstRow);
+            view.RowDefinitions.Add(secondRow);
+
+            Grid tbt = new PianoRollTray(pianoRoll).Build();
+            tbt.SetValue(Grid.RowProperty, 0);
+            tbt.SetValue(Grid.ColumnProperty, 0);
+
+            Grid pr = pianoRoll.Build();
+            pr.VerticalAlignment = VerticalAlignment.Bottom;
+            pr.SetValue(Grid.RowProperty, 1);
+            pr.SetValue(Grid.ColumnProperty, 0);
+
+            view.Children.Add(tbt);
+            view.Children.Add(pr);
+
+            window.Content = view;
         }
     }
 }
