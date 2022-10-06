e6da063cfbe1d812d2e460563fa451aadb9386fc
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-10-06 13:24:01 +0300

    Add scrolls

--- view/component/piano roll/PianoRoll.cs
+++ view/component/piano roll/PianoRoll.cs
@@ -1,4 +1,5 @@
-﻿using midi_sequencer.view.component.piano_roll;
+﻿using midi_sequencer.model;
+using midi_sequencer.view.component.piano_roll;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,40 +18,65 @@ namespace midi_sequencer.view.component
         private const int noteButtonWidth = 20;
         private const int noteButtonHeight = 20;
         private const int countOfNotes = 127;
+        private readonly int channel;
 
         private Grid pianoRoll;
 
         private Component pianoKeysBuilder;
-        private Component notesGridBuilder;
+        private NotesGrid notesGridBuilder;
 
-        public PianoRoll()
+        public PianoRoll(int channel)
         {
             this.pianoKeysBuilder = new PianoKeys(countOfNotes, noteButtonHeight);
             this.notesGridBuilder = new NotesGrid(countOfNotes, noteButtonHeight, noteButtonWidth);
             this.pianoRoll = new Grid();
+            this.channel = channel;
+        }
+
+        public List<NoteButton> GetNoteButtons()
+        {
+            return this.notesGridBuilder.GetNotes();
         }
 
         public Grid Build()
         {
+
             ColumnDefinition firstColumn = new ColumnDefinition();
             firstColumn.Width = new GridLength(100);
 
             ColumnDefinition secondColumn = new ColumnDefinition();
+
+            RowDefinition firstRow = new RowDefinition();
             RowDefinition secondRow = new RowDefinition();
 
             Grid pianoKeys = pianoKeysBuilder.Build();
             pianoKeys.SetValue(Grid.ColumnProperty, 0);
+            pianoKeys.SetValue(Grid.RowProperty, 1);
 
             Grid notesGrid = notesGridBuilder.Build();
             notesGrid.SetValue(Grid.ColumnProperty, 1);
+            notesGrid.SetValue(Grid.RowProperty, 1);
 
             this.pianoRoll.ColumnDefinitions.Add(firstColumn);
             this.pianoRoll.ColumnDefinitions.Add(secondColumn);
+            this.pianoRoll.RowDefinitions.Add(firstRow);
             this.pianoRoll.RowDefinitions.Add(secondRow);
             this.pianoRoll.Children.Add(notesGrid);
             this.pianoRoll.Children.Add(pianoKeys);
 
-            return pianoRoll;
+            ScrollViewer scroll = new ScrollViewer();
+            //scroll.HorizontalScrollBarVisibility = ScrollBarVisibility.Visible;
+
+            scroll.Content = this.pianoRoll;
+            Grid window = new Grid();
+            window.Children.Add(scroll);
+
+            //this.pianoRoll.Children.Add(tray);
+
+            return window;
         }
     }
 }
+
+// TODO SEPARATE SCROLL_VIEW NOTES_GRID INTO NotesGrid CLASS
+// TODO SCROLL FROM CODE
