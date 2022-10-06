a8ce14bc193dc8f63fb1b72bee4c30534cd95a14
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-10-06 13:20:09 +0300

    Create NoteButton model

---
+++ model/NoteButton.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace midi_sequencer.model
+{
+    internal class NoteButton
+    {
+        private int noteButtonWidth;
+        private int noteButtonHeight;
+        private int channel;
+
+        private Button noteButton;
+
+        public NoteButton(int channel, Button noteButton, int noteButtonWidth, int noteButtonHeight)
+        {
+            this.channel = channel;
+            this.noteButton = noteButton;
+            this.noteButtonWidth = noteButtonWidth;
+            this.noteButtonHeight = noteButtonHeight;
+        }
+
+        public Button GetControl()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetAbsoluteTime()
+        {
+            return (int)this.noteButton.Margin.Left / noteButtonWidth * 16;
+        }
+
+        public int GetDuration()
+        {
+            return (int)this.noteButton.Width / noteButtonWidth * 16;
+        }
+
+        public int GetNoteNumber()
+        {
+            return (int)this.noteButton.Height / noteButtonHeight;
+        }
+
+        public int GetChannel()
+        {
+            return this.channel;
+        }
+    }
+}
