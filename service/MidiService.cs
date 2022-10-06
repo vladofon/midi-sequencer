9cb95f4c9328321ad0e1764c8a69c5e8ec38be17
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-10-06 13:22:14 +0300

    Create service for MIDI

---
+++ service/MidiService.cs
@@ -0,0 +1,20 @@
+﻿using NAudio.Midi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace midi_sequencer.service
+{
+    internal class MidiService
+    {
+        public void AppendEndMarker(IList<MidiEvent> eventList)
+        {
+            long absoluteTime = 0;
+            if (eventList.Count > 0)
+                absoluteTime = eventList[eventList.Count - 1].AbsoluteTime;
+            eventList.Add(new MetaEvent(MetaEventType.EndTrack, 0, absoluteTime));
+        }
+    }
+}
