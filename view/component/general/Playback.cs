776f741be1d9cea50fa96abbcc7bbd63e7a688fa
k05ty <kostya.tsutsura@gmail.com>
2022-10-09 13:40:47 +0300

    Revert "Added PlaybackServise and control buttons in view.component.general.Playback"
    This reverts commit 114748d58db9887cf82ebb52f9ee86887dad9f7f.

--- view/component/general/Playback.cs
+++ view/component/general/Playback.cs
@@ -1,5 +1,4 @@
-﻿using midi_sequencer.service;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,14 +13,6 @@ namespace midi_sequencer.view.component.general
     {
         private Brush brush;
 
-        private const double buttonWidth = 50;
-        private const double buttonHeight = 50;
-        private const double distanceBetweenButtons = 5;
-
-        private PlaybackService playbackService = new(GlobalVars.midiOut, PlaybackService.OpenFile("C:\\Users\\kosty\\source\\repos\\midi-sequencer\\Test MIDI files\\d_dead\\d_dead.mid")); // Нужно перенести GlobalVars в класс PlaybackService (Если оставлять класс GlobalVars)
-        // PlaybackService.OpenFile("C:\\Users\\kosty\\source\\repos\\midi-sequencer\\Test MIDI files\\d_dead\\d_dead.mid")
-        // GlobalVars.midiEventCollection
-
         public Playback()
         {
             brush = Brushes.Blue;
@@ -29,61 +20,10 @@ namespace midi_sequencer.view.component.general
 
         public Grid Build()
         {
-            Button pauseButton = new()
-            {
-                Width = buttonWidth,
-                Height = buttonHeight,
-                HorizontalAlignment = HorizontalAlignment.Left,
-                Margin = new Thickness(distanceBetweenButtons, 0, 0, 0),
-                Content = "Pause"
-            };
-
-            Button playButton = new()
-            {
-                Width = buttonWidth,
-                Height = buttonHeight,
-                HorizontalAlignment = HorizontalAlignment.Left,
-                Margin = new Thickness(buttonWidth + distanceBetweenButtons * 2, 0, 0, 0),
-                Content = "Play"
-            };
-
-            Button stopButton = new()
-            {
-                Width = buttonWidth,
-                Height = buttonHeight,
-                HorizontalAlignment = HorizontalAlignment.Left,
-                Margin = new Thickness(buttonWidth * 2 + distanceBetweenButtons * 3, 0, 0, 0),
-                Content = "Stop"
-            };
-
-            pauseButton.Click += pauseButton_Click;
-            playButton.Click += playButton_Click;
-            stopButton.Click += stopButton_Click;
-
             Grid grid = new();
-
-            grid.Children.Add(pauseButton);
-            grid.Children.Add(playButton);
-            grid.Children.Add(stopButton);
-
             grid.Background = brush;
 
             return grid;
-        }
-
-        private void pauseButton_Click(object sender, RoutedEventArgs e)
-        {
-            playbackService.Pause();
-        }
-
-        private void playButton_Click(object sender, RoutedEventArgs e)
-        {
-            playbackService.Play();
-        }
-
-        private void stopButton_Click(object sender, RoutedEventArgs e)
-        {
-            playbackService.Stop();
         }
     }
 }
