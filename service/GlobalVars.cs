114748d58db9887cf82ebb52f9ee86887dad9f7f
k05ty <kostya.tsutsura@gmail.com>
2022-10-09 13:39:26 +0300

    Added PlaybackServise and control buttons in view.component.general.Playback Added class GlobalVars

---
+++ service/GlobalVars.cs
@@ -0,0 +1,15 @@
+﻿using NAudio.Midi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace midi_sequencer.service
+{
+    internal static class GlobalVars
+    {
+        static public MidiOut midiOut = new(0);
+        static public MidiEventCollection midiEventCollection = new(1, 128);
+    }
+}
