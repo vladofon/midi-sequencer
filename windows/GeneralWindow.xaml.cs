b360314c5da653d3534690f954553839f4584824
k05ty <kostya.tsutsura@gmail.com>
2022-11-19 17:38:45 +0200

    GeneralWindow opens when the program starts

--- windows/GeneralWindow.xaml.cs
+++ windows/GeneralWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using midi_sequencer.view;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,13 @@ namespace midi_sequencer.windows
     /// </summary>
     public partial class GeneralWindow : Window
     {
+        private DesignManager designManager;
+
         public GeneralWindow()
         {
             InitializeComponent();
+            this.designManager = new();
+            designManager.GeneralWindow(this);
         }
     }
 }
